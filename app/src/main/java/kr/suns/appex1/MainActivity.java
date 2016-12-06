6a04a9cb1d38d182d849500837c52b99eaf4edd2
jo <ktm1296@naver.com>
2016-12-06 15:16:21 +0900

    android commit test2

--- app/src/main/java/kr/suns/appex1/MainActivity.java
+++ app/src/main/java/kr/suns/appex1/MainActivity.java
@@ -2,6 +2,7 @@ package kr.suns.appex1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Stirng", Toast.LENGTH_LONG).show();
+
     }
 
     @Override
